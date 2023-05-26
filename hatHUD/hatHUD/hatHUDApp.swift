9bcc52e70b9f1979e8bae474e4deeb974ee7be5b
Ellie <mountzelizabeth@gmail.com>
2023-05-25 20:12:49 -0400

    added files

---
+++ hatHUD/hatHUD/hatHUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  hatHUDApp.swift
+//  hatHUD
+//
+//  Created by Elizabeth Mountz on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct hatHUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
